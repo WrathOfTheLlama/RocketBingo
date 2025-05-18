e2a820b724fc62027660f799a44b7cf85c20c4b4
WrathOfTheLlama <jmedlin920@gmail.com>
2025-05-17 22:34:09 -0700

    Update Server and Client with basic functionality, add index file to be used in server for webpage design

--- Server.py
+++ Server.py
@@ -1,37 +1,28 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-bingo_board =  ...
+@app.route(rule="/", methods=["GET", "POST"])
+def index():
+    # The GET endpoint
+    if request.method == "GET":
+        return "This is the GET Endpoint of flask API."
+    
+    # The POST endpoint
+    if request.method == "POST":
+        # accesing the passed payload
+        payload = request.get_json()
 
-@app.route('/create-board/<user_id>', methods=['POST'])
-def create_board(userid):
-    # Generate a bingo board (replace with actual bingo board logic)
-    board = ["Square"*25]
-    bingo_board[userid] = board
-    return jsonify({'message': 'Board created', 'board': board})
+        # capitalizing the text
+        cap_text = payload['text'].upper()
 
+        # Creating a proper response
+        response = {'cap-text': cap_text}
 
-@app.route('/mark-square', methods=['POST'])
-def mark_square():
-    # Get user id and the square number that user is marking from the request
-    team_id = request.json['team_id']
-    square_num = request.json['square_number']
-    ... # use the square_num to navigate the bingo board and mark the square for that player
+        # return the response as JSON
+        return jsonify(response)
+    
+    #return render_template('index.html')
 
-@app.route('/check-bingo', methods=['POST'])
-def check_bingo():
-    # Get user id from the request
-    user_id = request.json['user_id']
-    # Logic to check for bingo
-    return jsonify({'message': 'Bingo checked', 'user_id': user_id, 'bingo': True})
-
-@app.route('/get-board/<user_id>', methods=['GET'])
-def get_board(user_id):
-    if user_id in bingo_board:
-      return jsonify({'board': bingo_board[user_id]})
-    return jsonify({'message': 'No board found for this user'})
-
-
-if __name__ == 'main':
+if __name__ == "__main__":
     app.run(debug=True)
